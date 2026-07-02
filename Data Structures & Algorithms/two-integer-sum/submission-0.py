f5a534311c7bf9bd4aa86fb2f7204443af133f67
Mrityunjay Balkrishnan <66862112+Mj2603@users.noreply.github.com>
2026-07-02 20:10:17 +0530

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -1,13 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen= {}
-        
-        for i in range(len((nums))):
-            if target - nums[i] in seen:
-                return [seen.get(target - nums[i]), i]
-            seen[nums[i]]= i
-        return False
+        hmap= {}
 
-
-    
-        
+        for i in range(len(nums)):
+            if target-nums[i] in hmap:
+                return [hmap[target-nums[i]], i]
+            hmap[nums[i]]= i
