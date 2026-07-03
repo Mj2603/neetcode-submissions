5b69bb908af3d70081979d895739a261c08d2f0c
Mrityunjay Balkrishnan <66862112+Mj2603@users.noreply.github.com>
2026-07-03 14:39:05 +0530

    Update: top-k-elements-in-list - submission-0

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -1,16 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq= [[] for i in range(len(nums)+ 1)]
-        for num in nums:
-            count[num]= count.get(num,0)+ 1
+        n= len(nums)
+        hmap= {}
+        bucket= [[] for _ in range(n+1)]
 
-        for n, c in count.items():
-            freq[c].append(n)
+        for i in range(n):
+            hmap[nums[i]]= hmap.get(nums[i], 0) + 1
+
+        for num, freq in hmap.items():
+            bucket[freq].append(num)
 
         res= []
-        for i in range(len(freq)-1,0,-1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res)==k:
+        for i in range(n,-1,-1):
+            if bucket[i]:
+                res.extend(bucket[i])
+                if len(res)== k:
                     return res
+
